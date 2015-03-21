2e3903cc491f1930bb7f3adad8d2aec9fb63ee4a
Mellissa Cross <cros0324@umn.edu>
2015-03-21 14:32:28 -0500

    Split make_grid() and plot_data()

--- trendvis/gridwrapper.py
+++ trendvis/gridwrapper.py
@@ -2,25 +2,66 @@ from xgrid_ystack import XGrid
 from ygrid_xstack import YGrid
 
 
-def plot(xratios, yratios, figsize, xticks, yticks, main_axis, plotdata=None,
-         xlim=None, ylim=None,
-         startside='default', alternate_sides=True, line_kwargs={},
-         grid_cleanup=True, to_twin=None, axis_shift=None,
-         twinax_shift=None, onespine_forboth=False, logxscale='none',
-         logyscale='none', auto_spinecolor=True, tick_fontsize=10,
-         spinewidth=2):
+def make_grid(xratios, yratios, figsize, xticks, yticks, main_axis,
+              plotdata=None, xlim=None, ylim=None,
+              startside='default', alternate_sides=True, line_kwargs={},
+              grid_cleanup=True, to_twin=None, axis_shift=None,
+              twinax_shift=None, onespine_forboth=False, logxscale='none',
+              logyscale='none', auto_spinecolor=True, tick_fontsize=10,
+              spinewidth=2):
 
     """
-    Create a plot with multiple y (x) axes against the main axis, x (y).
+    Create a plot with a stack of multiple y (x) axes against
+        the main axis, x (y)
 
     Parameters
     ----------
+    xratios : int or list of ints
+        The relative sizes of the columns.  Not directly comparable
+        to yratios
+    yratios : int or list of ints
+        The relative sizes of the rows.  Not directly comparable to xratios
+    figsize : tuple of ints or floats
+        The figure dimensions in inches
+    xticks : list of tuples
+        List of (major, minor) tick mark multiples.  Used to set major and
+        minor locators.  One tuple per x axis
+    yticks : list of tuples
+        List of (major, minor) tick mark multiples.  Used to set major and
+        minor locators.  One tuple per y axis
+    main_axis : string
+        ['x'|'y'].  The common axis that all plots will share.
 
     Keyword Arguments
     -----------------
+    plotdata : list of lists of tuples
+        Default None. Tuple format: (x, y, color, {}, [ax inds within row/col])
+        One sublist per row or column (including twins).  To skip plotting on a
+        row or column, insert empty sublist at position corresponding to
+        the index of the row or column.
+    xlim : list of tuples of ints and/or floats
+        Default None.  List of (column, min, max).
+        If xdim is 1, then column is ignored.
+        Also, if only one x axis needs xlim, can just pass the tuple
+    ylim : List of tuples of ints and/or flaots
+        Default None.  List of (row, min, max).
+        If ydim is 1, then row is ignored.
+        Also, if only one y axis needs ylim, can just pass a tuple.
+    startside : string
+        Default 'default'.  For YGrid (`main_axis` = 'y') ['top'|'bottom'].
+        For XGrid (`main_axis` = 'x') ['left'|'right'].  The side that
+        topmost/leftmost stacked axis' spine will appear on if `grid_cleanup`
+        is True.
+    alternate_sides : Boolean
+        Default True.  [True|False].
+        Stacked axis spines alternate sides or are all on startside.
+    line_kwargs : dictionary
+
 
     Returns
     -------
+    grid : YGrid or XGrid object
+
 
 
     """
@@ -80,6 +121,19 @@ def plot(xratios, yratios, figsize, xticks, yticks, main_axis, plotdata=None,
 
 def plot_data(plotdata, grid, line_kwargs={}, auto_spinecolor=True):
     """
+    Plot a lot of data at once
+
+    Parameters
+    ----------
+    plotdata : list of lists of tuples
+
+    grid
+
+    Keyword Arguments
+    -----------------
+    line_kwargs : dictionary
+    auto_spinecolor : Boolean
+
     """
 
     # Initialize line defaults, update
@@ -93,9 +147,9 @@ def plot_data(plotdata, grid, line_kwargs={}, auto_spinecolor=True):
 
     default_linekwargs.update(line_kwargs)
 
-    for subgrid, datagroup in zip(grid.axes, plotdata):
+    for subgrid, subgrid_data in zip(grid.axes, plotdata):
         for ax_ind in range(0, grid.mainax_dim):
-            for dataset in datagroup:
+            for dataset in subgrid_data:
                 try:
                     if ax_ind in dataset[4]:
                         subgrid[ax_ind].errorbar(dataset[0], dataset[1],
