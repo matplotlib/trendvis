d2b214783fbe2e9e7a61fc4f5be56d4d64e06d83
Mellissa Cross <cros0324@umn.edu>
2015-07-11 12:18:36 -0500

    start testing folder

---
+++ trendvis/tests/test_trendvis.py
@@ -0,0 +1,13 @@
+from __future__ import division, absolute_import, print_function
+
+"""
+TrendVis testing
+
+???
+
+"""
+
+import trendvis
+
+from matplotlib.testing.decorators import image_comparison, cleanup
+import matplotlib.pyplot as plt
