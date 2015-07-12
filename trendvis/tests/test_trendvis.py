35f4f1da15775f39923ac019bd462c30e78a9291
Mellissa Cross <cros0324@umn.edu>
2015-07-11 23:48:29 -0500

    testing XGrid, YGrid init, cleanup grid; images

--- trendvis/tests/test_trendvis.py
+++ trendvis/tests/test_trendvis.py
@@ -9,5 +9,36 @@ TrendVis testing
 
 import trendvis
 
-from matplotlib.testing.decorators import image_comparison, cleanup
+from trendvis.testing import image_comparison
+
+from matplotlib.testing.decorators import cleanup
 import matplotlib.pyplot as plt
+from matplotlib.testing.noseclasses import KnownFailure
+
+
+@cleanup
+def test_xgrid_init():
+    trendvis.XGrid([2, 3, 1, 2])
+
+
+@cleanup
+def test_ygrid_init():
+    trendvis.YGrid([2, 3, 1, 2])
+
+
+@image_comparison(baseline_images=['xgrid_clean_001'])
+def test_xgrid_clean():
+    testgrid = trendvis.XGrid([2, 3, 1, 2])
+    testgrid.cleanup_grid()
+
+
+if __name__ == '__main__':
+    import nose
+    import sys
+
+    nose.main(addplugins=[KnownFailure()])
+
+    args = ['-s', '--with-doctest']
+    argv = sys.argv
+    argv = argv[:1] + args + argv[1:]
+    nose.runmodule(argv=argv, exit=False)
